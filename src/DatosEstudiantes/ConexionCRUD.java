42e4c44d7ee89abfcfe0c0e8c5f06e070d46f368
Giovanni-Lopez <alexisange001@gmail.com>
2021-05-07 23:07:36 -0600

    Se agrego la direccion del servidor, usuario, clave, driverConector, variable estatica para la conexion a la base de datos.

--- src/DatosEstudiantes/ConexionCRUD.java
+++ src/DatosEstudiantes/ConexionCRUD.java
@@ -1,9 +1,20 @@
 package DatosEstudiantes;
 
+import java.sql.*;
+
 /**
  *
  * @author Giovanni López
  */
 public class ConexionCRUD {
     
+    private final String servidor = "jdbc:mysql://127.0.0.1:3308/bd_institucion";
+    
+    private final String usuario = "root";
+    
+    private final String clave = "";
+    
+    private final String driverConector = "com.mysql.jdbc.Driver";
+    
+    private static Connection conexion;
 }
