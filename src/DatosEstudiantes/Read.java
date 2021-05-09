1d70dbf9b710261ff717f2d0434d452ea1a6bb18
David E. Flores <floresdavide0@gmail.com>
2021-05-09 10:20:01 -0600

    Se creo la estructura de la clase Read

--- src/DatosEstudiantes/Read.java
+++ src/DatosEstudiantes/Read.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DatosEstudiantes;
 
-/**
- *
- * @author df224
- */
+import java.sql.SQLException;
+
 public class Read {
     
+    public Read() throws SQLException{
+        
+        System.out.println("<< REGISTROS ALMACENADOS >>");
+        mostrarRegistros();
+        
+    }
+    
+    private void mostrarRegistros() throws SQLException{
+        
+        try{
+            ConexionCRUD utilerias = new ConexionCRUD();
+            String tabla = "tb_estudiante";
+            String camposTabla = "*";
+            String condicionBusqueda = "";
+            
+            utilerias.desplegarRegistros(tabla, camposTabla, condicionBusqueda);
+            
+        }catch(SQLException ex){
+            
+            System.out.println("Ha ocurrido el siguiente error: " + ex.getMessage());
+            
+        }finally{
+            
+            MenuPrincipal.desplegarMenu();
+        }
+    }
 }
